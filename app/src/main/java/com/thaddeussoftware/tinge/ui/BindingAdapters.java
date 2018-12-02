8241f2f5a80e3ecb96ea736ab4204a27abb5748c
Thaddeus <time2key@gmail.com>
2018-12-02 09:07:07 +0000

    Change size of light and group images on UI

---
+++ app/src/main/java/com/thaddeussoftware/tinge/ui/BindingAdapters.java
@@ -0,0 +1,23 @@
+package com.thaddeussoftware.tinge.ui;
+
+import android.databinding.BindingAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BindingAdapters {
+
+    @BindingAdapter("android:layout_width")
+    public static void setLayoutWidth(View view, float width) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = (int) width;
+        view.setLayoutParams(layoutParams);
+    }
+
+    @BindingAdapter("android:layout_height")
+    public static void setLayoutHeight(View view, float height) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) height;
+        view.setLayoutParams(layoutParams);
+    }
+
+}
