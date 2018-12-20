b50a7c4e86ef2e2839492c015f2e8a2ddd1297de
Thaddeus <time2key@gmail.com>
2018-12-20 14:02:11 +0000

    Add animations and current value text to SliderView

--- app/src/main/java/com/thaddeussoftware/tinge/ui/lights/SliderView.kt
+++ app/src/main/java/com/thaddeussoftware/tinge/ui/lights/SliderView.kt
@@ -1,7 +1,7 @@
 package com.thaddeussoftware.tinge.ui.lights
 
+import android.animation.Animator
 import android.content.Context
-import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
@@ -34,6 +34,24 @@ class SliderView @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyle: Int = 0): FrameLayout(context, attrs, defStyle) {
+
+    private val ANIMATION_DURATION_MIN_MAX_LABELS_IN_MS = 400L
+    private val ANIMATION_DURATION_MIN_MAX_LABELS_OUT_MS = 200L
+    private val ANIMATION_DURATION_MIN_MAX_LABELS_OUT_SINGLE_CLICK_MS = 50L
+
+    private val ANIMATION_DURATION_TOUCH_DOWN_MS = 100L
+    private val ANIMATION_DURATION_TOUCH_UP_MS = 400L
+    private val ANIMATION_HANDLE_SCALE_X_Y_TOUCH_DOWN = 1.3f
+
+    private val ANIMATION_DURATION_TOUCH_DOWN_SINGLE_CLICK_MS = 100L
+    private val ANIMATION_DURATION_TOUCH_UP_SINGLE_CLICK_MS = 400L
+    private val ANIMATION_HANDLE_SCALE_X_Y_TOUCH_DOWN_SINGLE_CLICK = 1.3f
+
+
+    private val AMOUNT_SLID_TO_HIDE_MAX_LABEL = 0.8f
+    private val AMOUNT_SLID_TO_RESHOW_MAX_LABEL = 0.75f
+    private val AMOUNT_SLID_TO_HIDE_MIN_LABEL = 0.2f
+    private val AMOUNT_SLID_TO_RESHOW_MIN_LABEL = 0.25f
 
     private val DEFAULT_TRACK_OPACITY = 0.65f
 
@@ -94,6 +112,29 @@ class SliderView @JvmOverloads constructor(
         }
 
     /**
+     * The 'on move' text shows a label up below the handle when the slider handle is moved.
+     *
+     * This value determines the maximum value that will be shown in this text, e.g. 100 if the
+     * on move text should show a percent symbol. This value will be formatted by
+     * [onMoveTextStringFormat].
+     * */
+    var onMoveTextMaximumAmount: Float = 1f
+        set(value) {
+            field = value
+        }
+
+    /**
+     * The 'on move' text shows a label up below the handle when the slider handle is moved.
+     *
+     * This value determines the format that the value will be shown in.
+     * See also [onMoveTextMaximumAmount].
+     * */
+    var onMoveTextStringFormat: String = ""
+        set(value) {
+            field = value
+        }
+
+    /**
      * Amount the current slider is slid:
      * If the slider is in the normal range, this value will be from 0 - 1
      * If [supportsOffValue] is true, and the slider is in the 'Off' position, this value will be -1
@@ -142,6 +183,35 @@ class SliderView @JvmOverloads constructor(
     private var hasBeenMovedEnoughInXDirectionToBeValidSide = false
 
     /**
+     * Whether there is currently an active touch held down on this view.
+     *
+     * Updated in [onTouchEvent] only, read in multiple other places.
+     * */
+    private var isTouchCurrentlyDown: Boolean = false
+
+    /**
+     * Set to true if the view has been animated into the touch-down state (with the handle bigger
+     * etc).
+     *
+     * This value changes when an animation begins to change the view into the touch-down or
+     * touch-up state.
+     *
+     * Changed and queried by [updateUiForWhetherTouchIsCurrentlyDown] only.
+     * */
+    private var isUiInTouchDownState = false
+
+    /**
+     * Set to true when the max label has been made visible, set to false when the max label has
+     * been made invisible to accomodate the on-move text.
+     * */
+    private var isMaxValueLabelAnimatedVisible = true
+    /**
+     * Set to true when the min label has been made visible, set to false when the min label has
+     * been made invisible to accomodate the on-move text.
+     * */
+    private var isMinValueLabelAnimatedVisible = false
+
+    /**
      * Array of colours set using [setHandleToAutoColors] that the handle should change between
      * depending on [slidAmount]
      * */
@@ -160,6 +230,10 @@ class SliderView @JvmOverloads constructor(
         startValueText = attributes.getString(R.styleable.SliderView_startValueText)
         endValueText = attributes.getString(R.styleable.SliderView_endValueText)
         offValueText = attributes.getString(R.styleable.SliderView_offValueText)
+
+        onMoveTextMaximumAmount = attributes.getFloat(R.styleable.SliderView_onMoveTextMaximumAmount, 1f)
+        onMoveTextStringFormat = attributes.getString(R.styleable.SliderView_onMoveTextStringFormat) ?: ""
+
         setTrackToColors(0xffaaaaaa.toInt(), 0xffaaaaaa.toInt())
         setHandleToAutoColors(0xffaaaaaa.toInt(), 0xffaaaaaa.toInt())
     }
@@ -225,19 +299,31 @@ class SliderView @JvmOverloads constructor(
         if (event?.actionMasked == MotionEvent.ACTION_CANCEL) {
             requestDisallowInterceptTouchEvent(false)
             hasBeenMovedEnoughInXDirectionToBeValidSide = false
+            isTouchCurrentlyDown = false
             return true
         }
         if (event?.actionMasked == MotionEvent.ACTION_UP) {
             requestDisallowInterceptTouchEvent(false)
             hasBeenMovedEnoughInXDirectionToBeValidSide = false
+            isTouchCurrentlyDown = false
 
             moveSliderPositionToTouchEvent(event)
+            if (!isUiInTouchDownState) {
+                // UI is not in a touch down state - this indicates that this ACTION_UP event
+                // is as a result of a quick click down and then up again, without moving the
+                // slider around in between the down and up:
+                animateInThenOutForSingleClick()
+                return true
+            }
         }
         if (event?.actionMasked == MotionEvent.ACTION_DOWN) {
+            isTouchCurrentlyDown = true
             touchDownX = event.x
         }
         if (event?.actionMasked == MotionEvent.ACTION_DOWN ||
                 event?.actionMasked == MotionEvent.ACTION_MOVE) {
+
+            isTouchCurrentlyDown = true
 
             // If has been moved enough in the x direction, disallow parent scrolling:
 
@@ -247,14 +333,130 @@ class SliderView @JvmOverloads constructor(
                 hasBeenMovedEnoughInXDirectionToBeValidSide = true
             }
 
-
             if (hasBeenMovedEnoughInXDirectionToBeValidSide) {
                 moveSliderPositionToTouchEvent(event)
             } else {
                 return true
             }
         }
+
+        updateUiOfMaxAndMinLabelVisibility()
+        updateUiForWhetherTouchIsCurrentlyDown()
         return true
+    }
+
+    /**
+     * Starts animations to either turn the view into the touch-down state (with the bigger handle
+     * etc) or the touch-up state (with the regular handle etc).
+     *
+     * Updates [isUiInTouchDownState].
+     * */
+    private fun updateUiForWhetherTouchIsCurrentlyDown() {
+        if (!isUiInTouchDownState && isTouchCurrentlyDown && hasBeenMovedEnoughInXDirectionToBeValidSide) {
+            // Animate to touch down state:
+            isUiInTouchDownState = true
+
+            binding.sliderHandleView.clearAnimation()
+            binding.sliderHandleView
+                    .animate()
+                    .scaleX(ANIMATION_HANDLE_SCALE_X_Y_TOUCH_DOWN)
+                    .scaleY(ANIMATION_HANDLE_SCALE_X_Y_TOUCH_DOWN)
+                    .setDuration(ANIMATION_DURATION_TOUCH_DOWN_MS)
+                    .setListener(null)
+
+            binding.handleBelowLabel.clearAnimation()
+            binding.handleBelowLabel
+                    .animate()
+                    .alpha(1f)
+                    .setDuration(ANIMATION_DURATION_TOUCH_DOWN_MS)
+                    .setListener(null)
+        }
+        else if (isUiInTouchDownState && !isTouchCurrentlyDown) {
+            // Animate to touch up state:
+            isUiInTouchDownState = false
+
+            binding.sliderHandleView
+                    .animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setDuration(ANIMATION_DURATION_TOUCH_UP_MS)
+                    .setListener(null)
+
+            binding.handleBelowLabel
+                    .animate()
+                    .alpha(0f)
+                    .setDuration(ANIMATION_DURATION_TOUCH_UP_MS)
+                    .setListener(null)
+        }
+
+    }
+
+    /**
+     * Called by [onTouchEvent] when the user single-clicks (touches down and then up without
+     * moving the touch first).
+     *
+     * Performs an animation that animates the touch handle big and then small again.
+     * */
+    private fun animateInThenOutForSingleClick() {
+
+        val firstAnimationDoneStartSecondAnimationListener = object: Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {}
+
+            override fun onAnimationCancel(p0: Animator?) {}
+
+            override fun onAnimationStart(p0: Animator?) {}
+
+            override fun onAnimationEnd(p0: Animator?) {
+
+                binding.sliderHandleView
+                        .animate()
+                        .scaleX(1f)
+                        .scaleY(1f)
+                        .setDuration(ANIMATION_DURATION_TOUCH_UP_SINGLE_CLICK_MS)
+                        .setListener(null)
+
+                binding.handleBelowLabel
+                        .animate()
+                        .alpha(0f)
+                        .setDuration(ANIMATION_DURATION_TOUCH_UP_SINGLE_CLICK_MS)
+                        .setListener(null)
+
+                updateUiOfMaxAndMinLabelVisibility()
+
+            }
+        }
+
+        binding.sliderHandleView.clearAnimation()
+        binding.sliderHandleView
+                .animate()
+                .scaleX(ANIMATION_HANDLE_SCALE_X_Y_TOUCH_DOWN_SINGLE_CLICK)
+                .scaleY(ANIMATION_HANDLE_SCALE_X_Y_TOUCH_DOWN_SINGLE_CLICK)
+                .setDuration(ANIMATION_DURATION_TOUCH_DOWN_SINGLE_CLICK_MS)
+                .setListener(firstAnimationDoneStartSecondAnimationListener)
+
+        binding.handleBelowLabel.clearAnimation()
+        binding.handleBelowLabel
+                .animate()
+                .alpha(1f)
+                .setDuration(ANIMATION_DURATION_TOUCH_DOWN_SINGLE_CLICK_MS)
+                .setListener(null)
+
+        if (slidAmount ?: 0f > AMOUNT_SLID_TO_HIDE_MAX_LABEL) {
+            isMaxValueLabelAnimatedVisible = false
+            binding.onRightTextView
+                    .animate()
+                    .alpha(0f)
+                    .setDuration(ANIMATION_DURATION_MIN_MAX_LABELS_OUT_SINGLE_CLICK_MS)
+                    .setListener(null)
+        }
+        if (slidAmount ?: 1f < AMOUNT_SLID_TO_HIDE_MIN_LABEL) {
+            isMinValueLabelAnimatedVisible = false
+            binding.onLeftTextView
+                    .animate()
+                    .alpha(0f)
+                    .setDuration(ANIMATION_DURATION_MIN_MAX_LABELS_OUT_SINGLE_CLICK_MS)
+                    .setListener(null)
+        }
     }
 
     /**
@@ -316,6 +518,7 @@ class SliderView @JvmOverloads constructor(
     private fun updateUIToCurrentSlidAmount() {
         if (slidAmount == null) {
             binding.sliderHandleView.visibility = GONE
+            binding.handleBelowLabel.text = ""
         } else {
 
             var horizontalBias = 0f
@@ -337,6 +540,53 @@ class SliderView @JvmOverloads constructor(
             constraintSet.applyTo(binding.constraintLayout)
 
             innerSetHandleToColor(getCurrentAutoColor())
+
+            binding.handleBelowLabel.text = String.format(onMoveTextStringFormat, slidAmount?.times(onMoveTextMaximumAmount))
+        }
+    }
+
+    /**
+     * Updates the visibility of the "Min" and "Max" labels, with animation.
+     *
+     * These are hidden if the slider is moved close to either edge, so that the value label below
+     * the slider is not obscured by the Min/Max labels.
+     * */
+    private fun updateUiOfMaxAndMinLabelVisibility() {
+        // Animate max label out:
+        if (slidAmount ?: 0f > AMOUNT_SLID_TO_HIDE_MAX_LABEL && isMaxValueLabelAnimatedVisible && isTouchCurrentlyDown) {
+            binding.onRightTextView
+                    .animate()
+                    .setDuration(ANIMATION_DURATION_MIN_MAX_LABELS_OUT_MS)
+                    .alpha(0f)
+                    .setListener(null)
+            isMaxValueLabelAnimatedVisible = false
+        }
+        // Animate max label back in:
+        else if (slidAmount ?: 0f < AMOUNT_SLID_TO_RESHOW_MAX_LABEL && !isMaxValueLabelAnimatedVisible || !isTouchCurrentlyDown) {
+            binding.onRightTextView
+                    .animate()
+                    .setDuration(ANIMATION_DURATION_MIN_MAX_LABELS_IN_MS)
+                    .alpha(1f)
+                    .setListener(null)
+            isMaxValueLabelAnimatedVisible = true
+        }
+        // Animate min label out:
+        if (slidAmount ?: 0f < AMOUNT_SLID_TO_HIDE_MIN_LABEL && isMinValueLabelAnimatedVisible && isTouchCurrentlyDown) {
+            binding.onLeftTextView
+                    .animate()
+                    .setDuration(ANIMATION_DURATION_MIN_MAX_LABELS_OUT_MS)
+                    .alpha(0f)
+                    .setListener(null)
+            isMinValueLabelAnimatedVisible = false
+        }
+        // Animate min label back in:
+        else if (slidAmount ?: 0f > AMOUNT_SLID_TO_RESHOW_MIN_LABEL && !isMinValueLabelAnimatedVisible || !isTouchCurrentlyDown) {
+            binding.onLeftTextView
+                    .animate()
+                    .setDuration(ANIMATION_DURATION_MIN_MAX_LABELS_IN_MS)
+                    .alpha(1f)
+                    .setListener(null)
+            isMinValueLabelAnimatedVisible = true
         }
     }
 
