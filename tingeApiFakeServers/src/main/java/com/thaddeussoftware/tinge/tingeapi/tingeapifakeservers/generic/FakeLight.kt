b26878745ce30738a2cc1b44434edaa953506d0e
Thaddeus <time2key@gmail.com>
2019-11-29 23:24:59 +0000

    Add initial FakeLight and FakeLightWishlist classes

--- tingeApiFakeServers/src/main/java/com/thaddeussoftware/tinge/tingeapi/tingeapifakeservers/generic/FakeLight.kt
+++ tingeApiFakeServers/src/main/java/com/thaddeussoftware/tinge/tingeapi/tingeapifakeservers/generic/FakeLight.kt
@@ -1,7 +1,30 @@
 package com.thaddeussoftware.tinge.tingeapi.tingeapifakeservers.generic
 
 /**
- * Represents a fake light.
+ * Represents a fake light in a [FakeServer] and holds all the current server state of the light.
  * */
-abstract class FakeLight {
+abstract class FakeLight(
+        internal val fakeLightWishlist: FakeLightWishlist
+) {
+
+    var displayName = fakeLightWishlist.displayName
+
+    var isReachable = fakeLightWishlist.isReachable
+
+    /**
+     * Brightness from 0 - 1
+     * */
+    var brightness = fakeLightWishlist.brightness
+
+    var isOn = fakeLightWishlist.isOn
+
+    /**
+     * Hue from 0 - 1
+     * */
+    var hue = fakeLightWishlist.hue
+
+    /**
+     * Saturation from 0 - 1
+     * */
+    var saturation = fakeLightWishlist.saturation
 }
--- tingeApiFakeServers/src/main/java/com/thaddeussoftware/tinge/tingeapi/tingeapifakeservers/generic/FakeLightWishlist.kt
+++ tingeApiFakeServers/src/main/java/com/thaddeussoftware/tinge/tingeapi/tingeapifakeservers/generic/FakeLightWishlist.kt
@@ -7,7 +7,7 @@ package com.thaddeussoftware.tinge.tingeapi.tingeapifakeservers.generic
  * This allows a [FakeLight] to be described without having to know about the implementation of the
  * [FakeServer] it is being added to.
  *
- * Any properties not set will use default values.
+ * Any properties not set will use default values - defaults to On / Full brightness / White.
  *
  * This base class only contains properties that are generic to any fake light regardless of
  * manufacturer. System / manufacturer-specific subclasses of [FakeLightWishlist] will exist to
@@ -18,4 +18,23 @@ class FakeLightWishlist {
     val displayName: String = ""
 
     val isReachable: Boolean = false
+
+    /**
+     * Brightness from 0 - 1
+     * */
+    var brightness: Float = 1f
+
+    var isOn: Boolean = true
+
+    /**
+     * Hue from 0 - 1
+     * */
+    var hue: Float = 0f
+
+    /**
+     * Saturation from 0 - 1
+     * */
+    var saturation: Float = 0f
+
+
 }
