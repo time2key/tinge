5616331b1c16550e8b56cae180980ddafe079243
Thaddeus <time2key@gmail.com>
2019-12-13 06:58:25 +0000

    Add initial auto generation of AutoGeneratedDispatcherModules
    For ModularMockWebServer.
    
    These are auto generated using annotation processing.
    
    Currently, there is no mechanism for calling these generated dispatcher modules though.

---
+++ modularMockServer/src/main/java/com/time2key/modularmockserver/ServerPathAnnotatedFunction.kt
@@ -0,0 +1,31 @@
+package com.time2key.modularmockserver
+
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
+
+/**
+ * Wrapper around a function inside a [DispatcherModule] that is annotated with [ServerPath]
+ * */
+abstract class ServerPathAnnotatedFunction() {
+
+    /**
+     * The simple class name of the [DispatcherModule] subclass that this belongs to. This is
+     * used to provide a helpful error message if something goes wrong.
+     * */
+    abstract val dispatcherModuleSimpleClassName: String
+
+    /**
+     * The simple name of the [ServerPath]-annotated function that this corresponds to. This is
+     * used to provide a helpful error message if something goes wrong.
+     * */
+    abstract val simpleFunctionName: String
+
+    abstract val matchingPathRegex: Regex
+
+    /**
+     * Higher number = higher priority, 0 is the default.
+     * */
+    abstract val priorityIfMultiplePathsMatch: Int
+
+    abstract fun evaluateRequest(request: RecordedRequest): MockResponse
+}
--- tingeApiFakeServers/src/main/java/com/thaddeussoftware/tinge/tingeapi/tingeapifakeservers/philipsHue/inner/HueFakeServerDispatcherModule.kt
+++ tingeApiFakeServers/src/main/java/com/thaddeussoftware/tinge/tingeapi/tingeapifakeservers/philipsHue/inner/HueFakeServerDispatcherModule.kt
@@ -2,12 +2,14 @@ package com.thaddeussoftware.tinge.tingeapi.tingeapifakeservers.philipsHue.inner
 
 import com.time2key.modularmockserver.DispatcherModule
 import com.time2key.modularmockserver.ServerPath
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
 
 
 class HueFakeServerDispatcherModule(): DispatcherModule() {
 
-    @ServerPath(".*i")
-    fun hi() {
-
+    @ServerPath(".*i(a*)/.*i")
+    fun hi(recordedRequest: RecordedRequest, captureGroup1: String): MockResponse? {
+        return null
     }
 }
